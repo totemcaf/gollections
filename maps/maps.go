62a9d913b40dac809c51f0dc39610083c2fb14a2
Charly Fau <carlos.fau@altscore.ai>
2022-07-14 17:31:48 -0300

    Add map a map values

--- maps/maps.go
+++ maps/maps.go
@@ -1,92 +1,105 @@
 package maps
 
+import "github.com/totemcaf/gollections/types"
+
 type Entry[K comparable, V any] struct {
-    Key   K
-    Value V
+	Key   K
+	Value V
 }
 
 func Entries[K comparable, V any](m map[K]V) []Entry[K, V] {
-    entries := make([]Entry[K, V], 0, len(m))
+	entries := make([]Entry[K, V], 0, len(m))
 
-    for k, v := range m {
-        entries = append(entries, Entry[K, V]{k, v})
-    }
+	for k, v := range m {
+		entries = append(entries, Entry[K, V]{k, v})
+	}
 
-    return entries
+	return entries
 }
 
 func Keys[K comparable, V any](m map[K]V) []K {
-    values := make([]K, 0, len(m))
+	values := make([]K, 0, len(m))
 
-    for k := range m {
-        values = append(values, k)
-    }
+	for k := range m {
+		values = append(values, k)
+	}
 
-    return values
+	return values
 }
 
 func Values[K comparable, V any](m map[K]V) []V {
-    values := make([]V, 0, len(m))
+	values := make([]V, 0, len(m))
 
-    for _, v := range m {
-        values = append(values, v)
-    }
+	for _, v := range m {
+		values = append(values, v)
+	}
 
-    return values
+	return values
 }
 
 // Clone copy a map entries into a new map
 func Clone[K comparable, V any](source map[K]V) map[K]V {
-    target := make(map[K]V, len(source))
+	target := make(map[K]V, len(source))
 
-    for k, v := range source {
-        target[k] = v
-    }
+	for k, v := range source {
+		target[k] = v
+	}
 
-    return target
+	return target
 }
 
 // ReduceKeys applies a reducer function to the keys of the map
 func ReduceKeys[Value any, Key comparable, Element any](
-    initialValue Value,
-    elements map[Key]Element,
-    reducer func(Value, Key) Value,
+	initialValue Value,
+	elements map[Key]Element,
+	reducer func(Value, Key) Value,
 ) Value {
-    accum := initialValue
+	accum := initialValue
 
-    for key := range elements {
-        accum = reducer(accum, key)
-    }
+	for key := range elements {
+		accum = reducer(accum, key)
+	}
 
-    return accum
+	return accum
 }
 
 // ReduceValues applies a reducer function to the values of the map
 func ReduceValues[Value any, Key comparable, Element any](
-    initialValue Value,
-    elements map[Key]Element,
-    reducer func(Value, Element) Value,
+	initialValue Value,
+	elements map[Key]Element,
+	reducer func(Value, Element) Value,
 ) Value {
-    accum := initialValue
+	accum := initialValue
 
-    for _, element := range elements {
-        accum = reducer(accum, element)
-    }
+	for _, element := range elements {
+		accum = reducer(accum, element)
+	}
 
-    return accum
+	return accum
 }
 
 // ReduceEntries applies a reducer function to the entries of the map
 func ReduceEntries[Value any, Key comparable, Element any](
-    initialValue Value,
-    elements map[Key]Element,
-    reducer func(Value, Key, Element) Value,
+	initialValue Value,
+	elements map[Key]Element,
+	reducer func(Value, Key, Element) Value,
 ) Value {
-    accum := initialValue
+	accum := initialValue
 
-    for key, element := range elements {
-        accum = reducer(accum, key, element)
-    }
+	for key, element := range elements {
+		accum = reducer(accum, key, element)
+	}
 
-    return accum
+	return accum
+}
+
+// Map maps values. Returns a new Map with same keys and values transformed by map function
+func Map[K comparable, V any, W any](m map[K]V, mapper types.Mapper[V, W]) map[K]W {
+	result := make(map[K]W, len(m))
+
+	for k, v := range m {
+		result[k] = mapper(v)
+	}
+
+	return result
 }
