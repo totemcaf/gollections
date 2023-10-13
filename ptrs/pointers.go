52496b3328494329dd190e6d7676e6d87adb4a92
Charly Fau <carlos.fau@altscore.ai>
2023-10-13 11:45:32 -0300

    Add ptr to zero and one

--- ptrs/pointers.go
+++ ptrs/pointers.go
@@ -1,5 +1,12 @@
 package ptrs
 
+import "golang.org/x/exp/constraints"
+
+// Number is a constraint that permits any numerical type.
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // SetIfPresent sets target to value if value is not nil
 func SetIfPresent[T any](target *T, value *T) {
 	if value != nil {
@@ -10,4 +17,26 @@ func SetIfPresent[T any](target *T, value *T) {
 // Ptr returns a pointer to a value
 func Ptr[T any](value T) *T {
 	return &value
+}
+
+// True returns a pointer to true
+func True() *bool {
+	return Ptr(true)
+}
+
+// False returns a pointer to false
+func False() *bool {
+	return Ptr(false)
+}
+
+// Zero returns a pointer to zero
+func Zero[T Number]() *T {
+	t := T(0)
+	return &t
+}
+
+// One returns a pointer to one
+func One[T Number]() *T {
+	t := T(1)
+	return &t
 }
--- ptrs/pointers_test.go
+++ ptrs/pointers_test.go
@@ -2,6 +2,7 @@ package ptrs
 
 import (
 	"github.com/stretchr/testify/require"
+	"reflect"
 	"testing"
 )
 
@@ -42,4 +43,53 @@ func TestSetIfPresent(t *testing.T) {
 			require.Equal(t, tt.expected, target)
 		})
 	}
+}
+
+func TestPtr(t *testing.T) {
+	value := 42
+
+	type args[T any] struct {
+		value T
+	}
+	type testCase[T any] struct {
+		name string
+		args args[T]
+		want *T
+	}
+	tests := []testCase[int]{
+		{
+			name: "int",
+			args: args[int]{
+				value: value,
+			},
+			want: &value,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Ptr(tt.args.value); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Ptr() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_True(t *testing.T) {
+	require.Equal(t, Ptr(true), True())
+	require.True(t, *True())
+}
+
+func Test_False(t *testing.T) {
+	require.Equal(t, Ptr(false), False())
+	require.False(t, *False())
+}
+
+func Test_Zero(t *testing.T) {
+	require.Equal(t, Ptr(0), Zero[int]())
+	require.Equal(t, 0.0, *Zero[float64]())
+}
+
+func Test_One(t *testing.T) {
+	require.Equal(t, Ptr[int32](1), One[int32]())
+	require.Equal(t, int32(1), *One[int32]())
 }
